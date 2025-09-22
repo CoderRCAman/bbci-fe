22e91ab24958d6b3de2521842aa8d69245e870f7
CoderRCAman <www.amank.o.12345@gmail.com>
2025-09-22 23:34:27 +0530

    some-updates

--- src/pages/Tab6/PMHInput.tsx
+++ src/pages/Tab6/PMHInput.tsx
@@ -1,119 +1,205 @@
-import { Dropdown } from 'primereact/dropdown'
-import { FloatLabel } from 'primereact/floatlabel'
-import { InputText } from 'primereact/inputtext'
-import { PERSONAL_MEDICAL_HISTORY_DB } from './Tab6'
-import { useState } from 'react'
+import { Dropdown } from "primereact/dropdown";
+import { FloatLabel } from "primereact/floatlabel";
+import { InputText } from "primereact/inputtext";
+import { PERSONAL_MEDICAL_HISTORY_DB } from "./Tab6";
+import { useEffect, useState } from "react";
 export default function PMHInput({
-    condition,
-    mode_of_treatment,
-    mode_of_diagnosis
-}:
-    {
-        condition: string,
-        mode_of_treatment: string[],
-        mode_of_diagnosis: string[]
-    }
-) {
-; 
-
-    return (
-        <div className="border rounded-md p-2 shadow">
-            <div className="space-y-7">
-                <h1 className="font-semibold text-slate-500 text-lg">{condition}</h1>
-                <div className="flex gap-4 items-center  text-md">
-                    <div className="space-x-2">
-                        <input type="radio" name="condition_status" value={1} />
-                        <span>YES </span>
-                    </div>
-                    <div className="space-x-2">
-                        <input type="radio" name="condition_status" value={2} />
-                        <span>NO </span>
-                    </div>
-                    <div className="space-x-2">
-                        <input type="radio" name="condition_status" value={2} />
-                        <span>DON'T KNOW </span>
-                    </div>
-                    <div className="space-x-2">
-                        <input type="radio" name="condition_status" value={2} />
-                        <span>REFUSED TO ANSWER</span>
-                    </div>
-
-                </div>
-                <div >
-                    <FloatLabel>
-                        <InputText
-                            keyfilter="int"
-                            className="border-1 p-2"
-                        />
-                        <label>Age at first diagnosis</label>
-                    </FloatLabel>
-
-                </div>
-                <div >
-                    <FloatLabel>
-                        <InputText
-                            className="border-1 p-2"
-                        />
-                        <label>Year of first diagnosis</label>
-                    </FloatLabel>
-
-                </div>
-                <div>
-                    <Dropdown
-                        optionLabel="name"
-                        optionValue="value"
-                        className="border-1"
-                        placeholder="Treatment Received"
-                        options={[
-                            { name: "YES", value: "1" },
-                            { name: "NO", value: "2" },
-                            { name: "DON'T KNOW", value: "8" },
-                            { name: "REFUSED TO ANSWER", value: "8" }
-                        ]}
-                    />
-                </div>
-                <div>
-                    <h1 className="font-semibold text-slate-500">Mode of treatment</h1>
-
-                    <div className="p-2 space-y-2">
-                        {
-                            mode_of_treatment.map(d => (
-                                <div className="flex gap-2">
-                                    <input type="checkbox" value={d} />
-                                    <p >{d}</p>
-                                </div>
-                            ))
-                        }
-                        <div className="flex gap-2">
-
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <h1 className="font-semibold text-slate-500">Mode of diagnosis</h1>
-                    <div className="p-2 space-y-2">
-                        {
-                            mode_of_diagnosis.map(d => (
-                                <div className="flex gap-2">
-                                    <input type="checkbox" value={d} />
-                                    <p >{d}</p>
-                                </div>
-                            ))
-                        }
-                        <div className='flex gap-5'>
-                            <p>Other specify</p>
-                            <input type="text" className='border-b-2 focus:outline-none focus:border-slate-500 w-[60%]' />
-                        </div>
-                        <div className="flex gap-2">
-                            <input type="checkbox" value={"Don't know"} />
-                            <p >Don't know</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
-
+  condition,
+  mode_of_treatment,
+  mode_of_diagnosis,
+  data,
+  updateStateData,
+}: {
+  condition: string;
+  mode_of_treatment: string[];
+  mode_of_diagnosis: string[];
+  data: PERSONAL_MEDICAL_HISTORY_DB;
+  updateStateData: (id: string, field: string, value: any) => void;
+}) {
+  console.log(data);
+  
+  return (
+    <div className="border rounded-md p-2 shadow">
+      <div className="space-y-7">
+        <h1 className="font-semibold text-slate-500 text-lg">{condition}</h1>
+        <div className="flex gap-4 items-center  text-md">
+          <div className="space-x-2">
+            <input
+              type="radio"
+              name="condition_status"
+              value={1}
+              checked={data?.["diagnosed"] === 1 ? true : false}
+              onChange={(e) =>
+                updateStateData(data.id, "diagnosed", parseInt(e.target.value))
+              }
+            />
+            <span>YES </span>
+          </div>
+          <div className="space-x-2">
+            <input
+              type="radio"
+              name="condition_status"
+              value={2}
+              checked={data?.["diagnosed"] === 2 ? true : false}
+              onChange={(e) =>
+                updateStateData(data.id, "diagnosed", parseInt(e.target.value))
+              }
+            />
+            <span>NO </span>
+          </div>
+          <div className="space-x-2">
+            <input
+              type="radio"
+              name="condition_status"
+              value={8}
+              checked={data?.["diagnosed"] === 8 ? true : false}
+              onChange={(e) =>
+                updateStateData(data.id, "diagnosed", parseInt(e.target.value))
+              }
+            />
+            <span>DON'T KNOW </span>
+          </div>
+          <div className="space-x-2">
+            <input
+              type="radio"
+              name="condition_status"
+              value={9}
+              checked={data?.["diagnosed"] === 9 ? true : false}
+              onChange={(e) =>
+                updateStateData(data.id, "diagnosed", parseInt(e.target.value))
+              }
+            />
+            <span>REFUSED TO ANSWER</span>
+          </div>
         </div>
-    )
+        <div>
+          <FloatLabel>
+            <InputText
+              disabled={data?.diagnosed !== 1}
+              keyfilter="int"
+              className="border-1 p-2"
+            />
+            <label>Age at first diagnosis</label>
+          </FloatLabel>
+        </div>
+        <div>
+          <FloatLabel>
+            <InputText
+              className="border-1 p-2"
+              disabled={data?.diagnosed !== 1}
+            />
+            <label>Year of first diagnosis</label>
+          </FloatLabel>
+        </div>
+        <div>
+          <Dropdown
+            disabled={data?.diagnosed !== 1}
+            optionLabel="name"
+            optionValue="value"
+            className="border-1"
+            placeholder="Treatment Received"
+            options={[
+              { name: "YES", value: "1" },
+              { name: "NO", value: "2" },
+              { name: "DON'T KNOW", value: "8" },
+              { name: "REFUSED TO ANSWER", value: "8" },
+            ]}
+          />
+        </div>
+        <div>
+          <h1 className="font-semibold text-slate-500">Mode of treatment</h1>
+
+          <div className="p-2 space-y-2">
+            {mode_of_treatment.map((d) => (
+              <div className="flex gap-2">
+                <input
+                  type="checkbox"
+                  name="mot"
+                  value={d}
+                  checked={data?.mode_of_treatment?.split("|").includes(d)}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      const newModes = data?.mode_of_treatment
+                        ? data.mode_of_treatment.split("|")
+                        : [];
+                      newModes.push(d);
+                      updateStateData(
+                        data.id,
+                        "mode_of_treatment",
+                        newModes.join("|")
+                      );
+                    } else {
+                      const newModes = data?.mode_of_treatment
+                        ? data.mode_of_treatment.split("|")
+                        : [];
+                      const filteredModes = newModes.filter((m) => m !== d);
+                      updateStateData(
+                        data.id,
+                        "mode_of_treatment",
+                        filteredModes.join("|")
+                      );
+                    }
+                  }}
+                />
+                <p>{d}</p>
+              </div>
+            ))}
+            <div className="flex gap-2"></div>
+          </div>
+        </div>
+        <div>
+          <h1 className="font-semibold text-slate-500">Mode of diagnosis</h1>
+          <div className="p-2 space-y-2">
+            {mode_of_diagnosis.map((d) => (
+              <div className="flex gap-2">
+                <input
+                  type="checkbox"
+                  value={d}
+                  checked={data?.mode_of_diagnosis?.split("|").includes(d)}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      const newModes = data?.mode_of_diagnosis
+                        ? data.mode_of_diagnosis.split("|")
+                        : [];
+                      newModes.push(d);
+                      updateStateData(
+                        data.id,
+                        "mode_of_diagnosis",
+                        newModes.join("|")
+                      );
+                    } else {
+                      const newModes = data?.mode_of_diagnosis
+                        ? data.mode_of_diagnosis.split("|")
+                        : [];
+                      const filteredModes = newModes.filter((m) => m !== d);
+                      updateStateData(
+                        data.id,
+                        "mode_of_diagnosis",
+                        filteredModes.join("|")
+                      );
+                    }
+                  }}
+                />
+                <p>{d}</p>
+              </div>
+            ))}
+            <div className="flex gap-5">
+              <p>Other specify</p>
+              <input
+                type="text"
+                className="border-b-2 focus:outline-none focus:border-slate-500 w-[60%]"
+                onChange={(e) =>{
+                    updateStateData(data.id, "mod_other", e.target.value);
+                }}
+              />
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" value={"Don't know"} />
+              <p>Don't know</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/utils/Sqlite.tsx
+++ src/utils/Sqlite.tsx
@@ -1,98 +1,108 @@
-import React, { useState, useEffect, createContext, useContext, PropsWithChildren } from 'react';
-import { Capacitor } from '@capacitor/core';
+import React, {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  PropsWithChildren,
+} from "react";
+import { Capacitor } from "@capacitor/core";
 import {
-    CapacitorSQLite,
-    SQLiteConnection,
-    SQLiteDBConnection,
-    capSQLiteSet,
-    DBSQLiteValues
-} from '@capacitor-community/sqlite';
+  CapacitorSQLite,
+  SQLiteConnection,
+  SQLiteDBConnection,
+  capSQLiteSet,
+  DBSQLiteValues,
+} from "@capacitor-community/sqlite";
 
 // Import the JeepSqlite component for the web platform
 import { JeepSqlite } from "jeep-sqlite/dist/components/jeep-sqlite";
 
 // Define the shape of a patient
 interface Patient {
-    id: number;
-    name: string;
-    age: number;
-    gender: string;
+  id: number;
+  name: string;
+  age: number;
+  gender: string;
 }
 
 // Define the shape of the context value
 interface SQLiteContextValue {
-    db: SQLiteDBConnection | null;
-    isLoading: boolean;
-    error: Error | null;
-    sqlite: SQLiteConnection | null
-    baseUrl: string | null,
-    conflictedList: any[],
-    setConflictedList: React.Dispatch<React.SetStateAction<any[]>>,
-    setBaseUrl: React.Dispatch<React.SetStateAction<string | null>>;
+  db: SQLiteDBConnection | null;
+  isLoading: boolean;
+  error: Error | null;
+  sqlite: SQLiteConnection | null;
+  baseUrl: string | null;
+  conflictedList: any[];
+  setConflictedList: React.Dispatch<React.SetStateAction<any[]>>;
+  setBaseUrl: React.Dispatch<React.SetStateAction<string | null>>;
 }
-0
+0;
 // Create the context with a default value
 const SQLiteContext = createContext<SQLiteContextValue>({
-    db: null,
-    isLoading: true,
-    error: null,
-    sqlite: null,
-    baseUrl: null,
-    conflictedList: [],
-    setBaseUrl: () => { },
-    setConflictedList: () => { }
+  db: null,
+  isLoading: true,
+  error: null,
+  sqlite: null,
+  baseUrl: null,
+  conflictedList: [],
+  setBaseUrl: () => {},
+  setConflictedList: () => {},
 });
 
 // Create a custom hook to use the context
 export const useSQLite = () => {
-    return useContext(SQLiteContext);
+  return useContext(SQLiteContext);
 };
 
 // The provider component that initializes and manages the database connection
-export const SQLiteProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
-    const [db, setDb] = useState<SQLiteDBConnection | null>(null);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [error, setError] = useState<Error | null>(null);
-    const sqlite = new SQLiteConnection(CapacitorSQLite);
-    const [baseUrl, setBaseUrl] = useState<string | null>('http://localhost:11142');
-    const [ConflictedList, setConflictedList] = useState<any[]>([]);
-    useEffect(() => {
-        const initDb = async () => {
-            try {
-                const platform = Capacitor.getPlatform();
+export const SQLiteProvider: React.FC<PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [db, setDb] = useState<SQLiteDBConnection | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+  const sqlite = new SQLiteConnection(CapacitorSQLite);
+  const [baseUrl, setBaseUrl] = useState<string | null>(
+    "http://localhost:11142"
+  );
+  const [ConflictedList, setConflictedList] = useState<any[]>([]);
+  useEffect(() => {
+    const initDb = async () => {
+      try {
+        const platform = Capacitor.getPlatform();
 
-                if (platform === 'web') {
-                    // Create the 'jeep-sqlite' Stencil component for the web platform
-                    customElements.define("jeep-sqlite", JeepSqlite);
-                    const jeepSqliteEl = document.createElement("jeep-sqlite");
-                    document.body.appendChild(jeepSqliteEl);
-                    await customElements.whenDefined("jeep-sqlite");
+        if (platform === "web") {
+          // Create the 'jeep-sqlite' Stencil component for the web platform
+          customElements.define("jeep-sqlite", JeepSqlite);
+          const jeepSqliteEl = document.createElement("jeep-sqlite");
+          document.body.appendChild(jeepSqliteEl);
+          await customElements.whenDefined("jeep-sqlite");
 
-                    // Initialize the Web store
-                    await sqlite.initWebStore();
-                }
+          // Initialize the Web store
+          await sqlite.initWebStore();
+        }
 
-                // Check if a connection already exists
-                const dbName = 'patientdb';
-                const isCon = await sqlite.isConnection(dbName, false);
-                let newDb: SQLiteDBConnection;
+        // Check if a connection already exists
+        const dbName = "patientdb";
+        const isCon = await sqlite.isConnection(dbName, false);
+        let newDb: SQLiteDBConnection;
 
-                if (isCon.result) {
-                    newDb = await sqlite.retrieveConnection(dbName, false);
-                } else {
-                    newDb = await sqlite.createConnection(
-                        dbName,
-                        false,
-                        'no-encryption',
-                        1,
-                        false
-                    );
-                }
+        if (isCon.result) {
+          newDb = await sqlite.retrieveConnection(dbName, false);
+        } else {
+          newDb = await sqlite.createConnection(
+            dbName,
+            false,
+            "no-encryption",
+            1,
+            false
+          );
+        }
 
-                await newDb.open();
+        await newDb.open();
 
-                // Create the patients table if it doesn't exist
-                const query = `
+        // Create the patients table if it doesn't exist
+        const query = `
                     CREATE TABLE IF NOT EXISTS patients (
                         id TEXT PRIMARY KEY NOT NULL ,
                         i_name TEXT  , 
@@ -110,7 +120,7 @@ export const SQLiteProvider: React.FC<PropsWithChildren<{}>> = ({ children }) =>
                         updated_by TEXT DEFAULT 'UNKNOWN'
                     );
                 `;
-                const query2 = `
+        const query2 = `
                     CREATE TABLE IF NOT EXISTS residential_history (
                         id TEXT PRIMARY KEY  NOT NULL , 
                         user_id TEXT NOT NULL , 
@@ -121,30 +131,46 @@ export const SQLiteProvider: React.FC<PropsWithChildren<{}>> = ({ children }) =>
                         state TEXT , 
                         code INTEGER
                     );
-                `
+                `;
+        const query3 = `
+                    CREATE TABLE IF NOT EXISTS personal_medical_history (
+                        id TEXT PRIMARY KEY NOT NULL ,
+                        diagnoss TEXT  , 
+                        diagnosed INTEGER ,  
+                        age_first_diagnosis INTEGER ,
+                        year_of_first_diagnosis TEXT ,
+                        treatment_received INTEGER ,
+                        mode_of_treatment TEXT ,
+                        mode_of_diagnosis TEXT ,
+                        mode_of_diagnosis_other TEXT ,
+                        user_id TEXT    
+                    );
+                
+                `;
 
-                //synch flag -> 0 1 2 
-                // 0 -> never synched 
-                // 1 -> synched
-                // 2 -> updated 
+        //synch flag -> 0 1 2
+        // 0 -> never synched
+        // 1 -> synched
+        // 2 -> updated
 
-                await newDb.execute(query);
-                await newDb.execute(query2) ;
+        await newDb.execute(query);
+        await newDb.execute(query2);
+        await newDb.execute(query3);
 
-                try {
-                    const migration1 = `
+        try {
+          const migration1 = `
                     CREATE TABLE IF NOT EXISTS tracksync (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     patient_id TEXT UNIQUE,
                     synch INT DEFAULT 0,
-                    FOREIGN KEY (patient_id) REFERENCES patients(id));`
-                    await newDb.execute(migration1);
-                } catch (error) {
-                    console.log('--------- tracksync table already exist! ---------');
-                }
+                    FOREIGN KEY (patient_id) REFERENCES patients(id));`;
+          await newDb.execute(migration1);
+        } catch (error) {
+          console.log("--------- tracksync table already exist! ---------");
+        }
 
-                try {
-                    const triger1 = `
+        try {
+          const triger1 = `
                     CREATE TRIGGER insert_tracksync_on_patient_insert
                     AFTER INSERT ON patients
                     FOR EACH ROW
@@ -152,22 +178,21 @@ export const SQLiteProvider: React.FC<PropsWithChildren<{}>> = ({ children }) =>
                         INSERT INTO tracksync (patient_id, synch)
                         VALUES (NEW.id, 0);
                     END;
-                    `
-                    await newDb.execute(triger1);
-                } catch (error) {
-                    console.log('--------- Trigger1 already exist! ---------');
-                }
+                    `;
+          await newDb.execute(triger1);
+        } catch (error) {
+          console.log("--------- Trigger1 already exist! ---------");
+        }
 
+        try {
+          const migration2 = "ALTER TABLE patients ADD COLUMN _rev TEXT;";
+          await newDb?.execute(migration2);
+        } catch (error) {
+          console.log("--------- _rev colum already exist ----------");
+        }
 
-                try {
-                    const migration2 = 'ALTER TABLE patients ADD COLUMN _rev TEXT;'
-                    await newDb?.execute(migration2);
-                } catch (error) {
-                    console.log("--------- _rev colum already exist ----------");
-                }
-
-                try {
-                    const query = `
+        try {
+          const query = `
                     CREATE TABLE IF NOT EXISTS residential_history (
                         id TEXT PRIMARY KEY NOT NULL ,
                         from_age INT  , 
@@ -179,41 +204,40 @@ export const SQLiteProvider: React.FC<PropsWithChildren<{}>> = ({ children }) =>
                         user_id TEXT 
                     );
                 `;
-                    await newDb?.execute(query);
-                } catch (error) {
+          await newDb?.execute(query);
+        } catch (error) {}
 
-                }
-
-
-                setDb(newDb);
-                setIsLoading(false);
-            } catch (err: any) {
-                console.error('SQLite DB initialization failed:', err);
-                setError(err);
-                setIsLoading(false);
-            }
-        };
-
-        initDb();
-
-        // Cleanup function to close the database connection
-        return () => {
-            if (db) {
-                db.close().catch(console.error);
-            }
-        };
-    }, []);
-
-    const value = {
-        db, isLoading, error, sqlite,
-        baseUrl, setBaseUrl,
-        conflictedList: ConflictedList,
-        setConflictedList
+        setDb(newDb);
+        setIsLoading(false);
+      } catch (err: any) {
+        console.error("SQLite DB initialization failed:", err);
+        setError(err);
+        setIsLoading(false);
+      }
     };
 
-    return (
-        <SQLiteContext.Provider value={value}>
-            {children}
-        </SQLiteContext.Provider>
-    );
+    initDb();
+
+    // Cleanup function to close the database connection
+    return () => {
+      if (db) {
+        db.close().catch(console.error);
+      }
+    };
+  }, []);
+
+  const value = {
+    db,
+    isLoading,
+    error,
+    sqlite,
+    baseUrl,
+    setBaseUrl,
+    conflictedList: ConflictedList,
+    setConflictedList,
+  };
+
+  return (
+    <SQLiteContext.Provider value={value}>{children}</SQLiteContext.Provider>
+  );
 };
